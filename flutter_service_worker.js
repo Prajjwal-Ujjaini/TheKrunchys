a548224f1b7293c2539a4e51c26ea3c7ad8b4d8d
prajjwal ujjaini <prajjwalujjaini@gmail.com>
2023-03-06 00:10:31 +0530

    New Design + little Responsive

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,15 +3,24 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"assets/AssetManifest.json": "766188e38db1bd5e71bfccea1563b9d9",
-"assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
+  "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"assets/AssetManifest.json": "68729c88baef37294fb710710e8c9b27",
+"assets/FontManifest.json": "316a636bcc17db9bf8d0e5e78901c58b",
 "assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
+"assets/assets/images/homeMainCropped.png": "b58fa61b056f2bcfcda3ad403d6dbc8c",
+"assets/assets/images/background_new.png": "b446e7224c34f1e1ebc28995abe670ee",
+"assets/assets/images/storeItem.png": "f89f855f980fcb5764944395b1b28d8a",
 "assets/assets/images/squirrel_farming2.png": "710209f1b82b9cfa202d3d93e5a79c2c",
 "assets/assets/images/bottom_image.jpg": "16c1e70821224de89003be7693fad5e6",
 "assets/assets/images/farmer_planting.png": "5f94cde078d8f3d7a162c53c204caa06",
+"assets/assets/images/homeMainOrg.png": "a8cd75d6044a6a550e753d132822baae",
+"assets/assets/images/background.png": "51ed6f5708702594469bdbc343237ca0",
 "assets/assets/images/squirrel_farming.png": "d2ce24b98d82b7d80bfad0674c95c528",
-"assets/NOTICES": "2a5f5df707c7a9ed9fd46536f415f73a",
+"assets/assets/images/halfCircle.png": "234b7056cd3f5e38240fea69523d502d",
+"assets/assets/images/homeMain.jpeg": "60d0a9528b09f9f4cdd55a90fa0e556e",
+"assets/NOTICES": "4ad2b42f60f67383ebbb4b2e878b88ea",
+"assets/packages/social_media_flutter/fonts/icons.ttf": "a2943704265adec89858d6d95150a109",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/shaders/ink_sparkle.frag": "c8ad7de9075f116dc846bb038da55061",
 "manifest.json": "3f156636c1be304be76435b797750fd9",
@@ -19,10 +28,14 @@ const RESOURCES = {
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
-"index.html": "904d1890b9af1db321e96c442516a989",
-"/": "904d1890b9af1db321e96c442516a989",
-"main.dart.js": "cc83e4a11a6b1df7af703ff492c77231",
-"version.json": "7cd4e7365075500bb968fccdfaeaa77f"
+"index.html": "704663aa2623a8b5f5174cd336d41373",
+"/": "704663aa2623a8b5f5174cd336d41373",
+"main.dart.js": "7e7c25f60f0f70291492b5edb9c2b9c4",
+"version.json": "7cd4e7365075500bb968fccdfaeaa77f",
+"canvaskit/canvaskit.wasm": "bf50631470eb967688cca13ee181af62",
+"canvaskit/canvaskit.js": "2bc454a691c631b07a9307ac4ca47797",
+"canvaskit/profiling/canvaskit.wasm": "95a45378b69e77af5ed2bc72b2209b94",
+"canvaskit/profiling/canvaskit.js": "38164e5a72bdad0faa4ce740c9b8e564"
 };
 
 // The application shell files that are downloaded before a service worker can
